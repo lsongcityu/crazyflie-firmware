c0791643da5e7a134b39569a676f27c614201cd9
lsongcityu <157107254+lsongcityu@users.noreply.github.com>
2026-06-22 16:45:17 -0400

    Add Songnan's SO(3) controller and Flip function; Remove Supervisor and CollisionAvoidance

--- src/modules/src/stabilizer.c
+++ src/modules/src/stabilizer.c
@@ -25,6 +25,8 @@
  */
 #define DEBUG_MODULE "STAB"
 
+#define FLIP_FCN
+
 #include <math.h>
 
 #include "FreeRTOS.h"
@@ -70,6 +72,10 @@ static sensorData_t sensorData;
 static state_t state;
 static control_t control;
 
+static float attitude_control_limit;
+static float idle_thrust;
+bool thrust_flag;
+
 static motors_thrust_uncapped_t motorThrustUncapped;
 static motors_thrust_uncapped_t motorThrustBatCompUncapped;
 static motors_thrust_pwm_t motorPwm;
@@ -84,6 +90,151 @@ static STATS_CNT_RATE_DEFINE(stabilizerRate, 500);
 static rateSupervisor_t rateSupervisorContext;
 static bool rateWarningDisplayed = false;
 SemaphoreHandle_t xRateSupervisorSemaphore;
+
+static float kp_xy = 6000;
+static float kp_xy_temp = 6000;
+static float kp_z = 6000;
+static float kp_z_temp = 6000;
+
+static float norm_tau_omega_limit = 100.0f;
+
+// static float angle_error_threshold = 1.57f;
+// static float angle_error_velocity = 300.0f;
+
+static float kd_xy = 10;
+static float kd_z = 10;
+
+static float tau_x_offset = 0.0f;
+static float tau_y_offset = 0.0f;
+static float tau_z_offset = 0.0f;
+
+static float tau_x = 0.0f;
+static float tau_y = 0.0f;
+static float tau_z = 0.0f;
+
+static float omega_x = 0.0f;
+static float omega_y = 0.0f;
+static float omega_z = 0.0f;
+
+static float qw_desired = 1.0f;
+static float qx_desired = 0.0f;
+static float qy_desired = 0.0f;
+static float qz_desired = 0.0f;
+
+static float qw_desired_delay = 1.0f;
+static float qx_desired_delay = 0.0f;
+static float qy_desired_delay = 0.0f;
+static float qz_desired_delay = 0.0f;
+
+uint32_t timestamp_setpoint = 0;
+
+static float external_loop_freq = 100.0f;
+// static uint32_t time_gap_setpoint = 10000;
+
+#ifdef FLIP_FCN
+float flip_thrust;
+static int16_t flip_roll = 10000;
+float flip_thrust = 1380;
+float switch_angle = 130;
+#endif
+
+float limint16(float in)
+{
+  if (in > 32000.0f)
+    return 32000.0f;
+  else if (in < -32000.0f)
+    return -32000.0f;
+  else
+    return in;
+}
+
+float lim_num(float in, float num)
+{
+  if (in > num)
+    return num;
+  else if (in < -num)
+    return -num;
+  else
+    return in;
+}
+
+// Function to determine if two quaternions represent the same attitude
+bool same_attitude(float w, float x, float y, float z, float w_d, float x_d, float y_d, float z_d)
+{
+  // Calculate the dot product of the two quaternions
+  float dot_product = w * w_d + x * x_d + y * y_d + z * z_d;
+  // Check if the absolute value of the dot product is close to 1
+  return fabsf(dot_product) > 0.999999f;
+}
+
+void pcontrol(float w, float x, float y, float z, float w_d, float x_d,
+  float y_d, float z_d, float *tau_x, float *tau_y, float *tau_z)
+{
+
+if (same_attitude(w, x, y, z, w_d, x_d, y_d, z_d))
+{
+*tau_x = 0.0F;
+*tau_y = 0.0F;
+*tau_z = 0.0F;
+}
+else
+{
+float axang1;
+float axang2;
+float axang3;
+float axang4;
+float rot1;
+float temp_1;
+
+temp_1 = ((w * w_d + x * x_d) + y * y_d) + z * z_d;
+
+if (temp_1 == 1.0F)
+{
+/*  axang = [0, 0, 1, 0]; */
+axang1 = 0.0F;
+axang2 = 0.0F;
+axang3 = 1.0F;
+axang4 = 0.0F;
+}
+else
+{
+
+axang4 = sqrtf(1.0F - temp_1 * temp_1);
+axang1 = (((w * x_d - w_d * x) + y * z_d) - y_d * z) / axang4;
+axang2 = (((w * y_d - w_d * y) - x * z_d) + x_d * z) / axang4;
+axang3 = (((w * z_d - w_d * z) + x * y_d) - x_d * y) / axang4;
+axang4 = 2.0F * acosf(temp_1);
+}
+
+if (axang4 > 3.1415926535897931f)
+{
+axang4 = 6.28318548F - axang4;
+axang1 = -axang1;
+axang2 = -axang2;
+axang3 = -axang3;
+}
+else if (axang4 < -3.1415926535897931f)
+{
+axang4 += 6.28318548F + axang4;
+axang1 = -axang1;
+axang2 = -axang2;
+axang3 = -axang3;
+}
+
+rot1 = axang1 * axang4;
+axang1 = axang2 * axang4;
+temp_1 = axang3 * axang4;
+
+axang4 = (rot1 * x + axang1 * y) + temp_1 * z;
+axang2 = (rot1 * w - temp_1 * y) + axang1 * z;
+axang3 = (axang1 * w + temp_1 * x) - rot1 * z;
+temp_1 = (temp_1 * w - axang1 * x) + rot1 * y;
+
+*tau_x = ((w * axang2 - y * temp_1) + x * axang4) + z * axang3;
+*tau_y = ((w * axang3 + x * temp_1) + y * axang4) - z * axang2;
+*tau_z = ((w * temp_1 - x * axang3) + y * axang2) + z * axang4;
+}
+}
 
 static struct {
   // position - mm
@@ -133,6 +284,37 @@ static void calcSensorToOutputLatency(const sensorData_t *sensorData)
   inToOutLatency = outTimestamp - sensorData->interruptTimestamp;
 }
 
+void eul2quat_my(float yaw, float pitch, float roll,
+  float *w, float *x, float *y, float *z)
+{
+float c1c2;
+float c_idx_0;
+float c_idx_1;
+float c_idx_2;
+float s1s2;
+float s_idx_0;
+float s_idx_1;
+float s_idx_2;
+s_idx_0 = yaw / 2.0F;
+s_idx_1 = pitch / 2.0F;
+s_idx_2 = roll / 2.0F;
+c_idx_0 = cosf(s_idx_0);
+s_idx_0 = sinf(s_idx_0);
+c_idx_1 = cosf(s_idx_1);
+s_idx_1 = sinf(s_idx_1);
+c_idx_2 = cosf(s_idx_2);
+s_idx_2 = sinf(s_idx_2);
+c1c2 = c_idx_0 * c_idx_1;
+s1s2 = s_idx_0 * s_idx_1;
+c_idx_0 *= s_idx_1;
+s_idx_1 = s_idx_0 * c_idx_1;
+*w = c1c2 * c_idx_2 + s1s2 * s_idx_2;
+*x = c1c2 * s_idx_2 - s1s2 * c_idx_2;
+*y = c_idx_0 * c_idx_2 + s_idx_1 * s_idx_2;
+*z = s_idx_1 * c_idx_2 - c_idx_0 * s_idx_2;
+}
+
+
 static void compressState()
 {
   stateCompressed.x = state.position.x * 1000.0f;
@@ -160,20 +342,20 @@ static void compressState()
   stateCompressed.rateYaw = sensorData.gyro.z * deg2millirad;
 }
 
-static void compressSetpoint()
-{
-  setpointCompressed.x = setpoint.position.x * 1000.0f;
-  setpointCompressed.y = setpoint.position.y * 1000.0f;
-  setpointCompressed.z = setpoint.position.z * 1000.0f;
+// static void compressSetpoint()
+// {
+//   setpointCompressed.x = setpoint.position.x * 1000.0f;
+//   setpointCompressed.y = setpoint.position.y * 1000.0f;
+//   setpointCompressed.z = setpoint.position.z * 1000.0f;
 
-  setpointCompressed.vx = setpoint.velocity.x * 1000.0f;
-  setpointCompressed.vy = setpoint.velocity.y * 1000.0f;
-  setpointCompressed.vz = setpoint.velocity.z * 1000.0f;
+//   setpointCompressed.vx = setpoint.velocity.x * 1000.0f;
+//   setpointCompressed.vy = setpoint.velocity.y * 1000.0f;
+//   setpointCompressed.vz = setpoint.velocity.z * 1000.0f;
 
-  setpointCompressed.ax = setpoint.acceleration.x * 1000.0f;
-  setpointCompressed.ay = setpoint.acceleration.y * 1000.0f;
-  setpointCompressed.az = setpoint.acceleration.z * 1000.0f;
-}
+//   setpointCompressed.ax = setpoint.acceleration.x * 1000.0f;
+//   setpointCompressed.ay = setpoint.acceleration.y * 1000.0f;
+//   setpointCompressed.az = setpoint.acceleration.z * 1000.0f;
+// }
 
 void stabilizerInit(StateEstimatorType estimator)
 {
@@ -203,7 +385,7 @@ bool stabilizerTest(void)
   pass &= controllerTest();
   pass &= powerDistributionTest();
   pass &= motorsTest();
-  pass &= collisionAvoidanceTest();
+  // pass &= collisionAvoidanceTest();
 
   return pass;
 }
@@ -311,6 +493,14 @@ static void stabilizerTask(void* param)
   xRateSupervisorSemaphore = xSemaphoreCreateBinary();
   STATIC_MEM_TASK_CREATE(rateSupervisorTask, rateSupervisorTask, RATE_SUPERVISOR_TASK_NAME, NULL, RATE_SUPERVISOR_TASK_PRI);
 
+  idle_thrust = 1500.0f;
+
+  attitude_control_limit = 1300.0f;
+  thrust_flag = true;
+  
+  float tau_omega_x = 0.0f;
+  float tau_omega_y = 0.0f;
+  float norm_tau_omega = sqrtf(tau_omega_x * tau_omega_x + tau_omega_y * tau_omega_y);
   while(1) {
     // The sensor should unlock at 1kHz
     sensorsWaitDataReady();
@@ -337,16 +527,149 @@ static void stabilizerTask(void* param)
 
       // Critical for safety, be careful if you modify this code!
       // Let the supervisor update it's view of the current situation
-      supervisorUpdate(&sensorData, &setpoint, stabilizerStep);
+      // supervisorUpdate(&sensorData, &setpoint, stabilizerStep);
 
       // Let the collision avoidance module modify the setpoint, if needed
-      collisionAvoidanceUpdateSetpoint(&setpoint, &sensorData, &state, stabilizerStep);
+      // collisionAvoidanceUpdateSetpoint(&setpoint, &sensorData, &state, stabilizerStep);
 
       // Critical for safety, be careful if you modify this code!
       // Let the supervisor modify the setpoint to handle exceptional conditions
-      supervisorOverrideSetpoint(&setpoint);
+      // supervisorOverrideSetpoint(&setpoint);
 
-      controller(&control, &setpoint, &sensorData, &state, stabilizerStep);
+      // controller(&control, &setpoint, &sensorData, &state, stabilizerStep);
+      // disable P controller when thrust is equal to attitude_control_limit
+      if (fabsf(setpoint.thrust - attitude_control_limit) < 10.0f)
+      {
+        kp_xy_temp = 0.0f;
+        kp_z_temp = 0.0f;
+      }
+      else
+      {
+        kp_xy_temp = kp_xy;
+        kp_z_temp = kp_z;
+      }
+
+      if (timestamp_setpoint == setpoint.timestamp)
+      {
+        // no control input is received
+        ;
+      }
+      else
+      {
+        // control input is received
+        if (setpoint.timestamp > timestamp_setpoint)
+          // time_gap_setpoint = setpoint.timestamp - timestamp_setpoint;
+
+          timestamp_setpoint = setpoint.timestamp;
+
+        qw_desired_delay = qw_desired;
+        qx_desired_delay = qx_desired;
+        qy_desired_delay = qy_desired;
+        qz_desired_delay = qz_desired;
+
+        // compute desired quat
+        eul2quat_my(setpoint.attitudeRate.yaw * -0.0174532925199433f,
+                    setpoint.attitude.pitch * -0.0174532925199433f,
+                    setpoint.attitude.roll * 0.0174532925199433f,
+                    &qw_desired,
+                    &qx_desired,
+                    &qy_desired,
+                    &qz_desired);
+
+        pcontrol(qw_desired_delay,
+                 qx_desired_delay,
+                 qy_desired_delay,
+                 qz_desired_delay,
+                 qw_desired,
+                 qx_desired,
+                 qy_desired,
+                 qz_desired,
+                 &omega_x, &omega_y, &omega_z);
+
+        // // desired angular rate in degrees
+        omega_x = omega_x * 57.2957795130823f * external_loop_freq;
+        omega_y = omega_y * 57.2957795130823f * external_loop_freq;
+        omega_z = omega_z * 57.2957795130823f * external_loop_freq;
+
+        omega_x = lim_num(omega_x, 300);
+        omega_y = lim_num(omega_y, 300);
+        omega_z = lim_num(omega_z, 300);
+      }
+
+      if (fabsf(setpoint.thrust - idle_thrust) < 10.0f)
+      {
+        control.thrust = 500.0f;
+        control.roll = 0.0f;
+        control.pitch = 0.0f;
+        control.yaw = 0.0f;
+      }
+      else if (setpoint.thrust >= 10.0f)
+      {
+
+        pcontrol(state.attitudeQuaternion.w,
+                 state.attitudeQuaternion.x,
+                 state.attitudeQuaternion.y,
+                 state.attitudeQuaternion.z,
+                 qw_desired,
+                 qx_desired,
+                 qy_desired,
+                 qz_desired,
+                 &tau_x, &tau_y, &tau_z);
+
+        // float angle_error = sqrtf(tau_x * tau_x + tau_y * tau_y + tau_z * tau_z);
+
+        // if (angle_error > angle_error_threshold)
+        // {
+        //   omega_x = (tau_x/angle_error) * angle_error_velocity;
+        //   omega_y = (tau_y/angle_error) * angle_error_velocity;
+        //   omega_z = (tau_z/angle_error) * angle_error_velocity;
+        // }
+        // else
+        // {
+        //   omega_x = 0.0f;
+        //   omega_y = 0.0f;
+        //   omega_z = 0.0f;
+        // }
+
+        tau_x = tau_x + tau_x_offset;
+        tau_y = tau_y + tau_y_offset;
+        tau_z = tau_z + tau_z_offset;
+
+        tau_omega_x = omega_x - sensorData.gyro.x;
+        tau_omega_y = omega_y - sensorData.gyro.y;
+        norm_tau_omega = sqrtf(tau_omega_x * tau_omega_x + tau_omega_y * tau_omega_y);
+
+        if (norm_tau_omega > norm_tau_omega_limit)
+        {
+          tau_omega_x = tau_omega_x / norm_tau_omega * norm_tau_omega_limit;
+          tau_omega_y = tau_omega_y / norm_tau_omega * norm_tau_omega_limit;
+        }
+
+        control.thrust = setpoint.thrust;
+        control.roll = (int16_t)limint16(tau_x * kp_xy_temp + tau_omega_x * kd_xy);
+        control.pitch = -(int16_t)limint16(tau_y * kp_xy_temp + tau_omega_y * kd_xy);
+        control.yaw = -(int16_t)limint16(tau_z * kp_z + (omega_z - sensorData.gyro.z) * kd_z);
+#ifdef FLIP_FCN
+        if (fabsf(flip_thrust - setpoint.thrust) < 2.0f)
+        {
+          if (state.attitude.roll > -30 && state.attitude.roll < switch_angle)
+            control.roll = flip_roll;
+          else
+            control.roll = -flip_roll;
+
+          control.thrust = 1000.0f;
+          control.pitch = 0;
+          control.yaw = 0;
+        }
+#endif
+      }
+      else
+      {
+        control.thrust = 0.0f;
+        control.roll = 0.0f;
+        control.pitch = 0.0f;
+        control.yaw = 0.0f;
+      }
 
       // Critical for safety, be careful if you modify this code!
       // The supervisor will already set thrust to 0 in the setpoint if needed, but to be extra sure prevent motors from running.
@@ -361,7 +684,7 @@ static void stabilizerTask(void* param)
 
       // Compute compressed log formats
       compressState();
-      compressSetpoint();
+      // compressSetpoint();
 
 #ifdef CONFIG_DECK_USD
       // Log data to uSD card if configured
@@ -399,6 +722,31 @@ PARAM_ADD_CORE(PARAM_UINT8, estimator, &estimatorType)
  * @brief Controller type Auto select(0), PID(1), Mellinger(2), INDI(3), Brescianini(4), Lee(5) (Default: 0)
  */
 PARAM_ADD_CORE(PARAM_UINT8, controller, &controllerType)
+
+PARAM_ADD(PARAM_FLOAT, acl, &attitude_control_limit)
+
+PARAM_ADD(PARAM_FLOAT, kpxy, &kp_xy)
+PARAM_ADD(PARAM_FLOAT, kpz, &kp_z)
+PARAM_ADD(PARAM_FLOAT, kdxy, &kd_xy)
+PARAM_ADD(PARAM_FLOAT, kdz, &kd_z)
+PARAM_ADD(PARAM_FLOAT, exfreq, &external_loop_freq)
+
+// PARAM_ADD(PARAM_FLOAT, aet, &angle_error_threshold)
+// PARAM_ADD(PARAM_FLOAT, aev, &angle_error_velocity)
+
+PARAM_ADD(PARAM_FLOAT, qxo, &tau_x_offset)
+PARAM_ADD(PARAM_FLOAT, qyo, &tau_y_offset)
+PARAM_ADD(PARAM_FLOAT, qzo, &tau_z_offset)
+
+PARAM_ADD(PARAM_FLOAT, ntol, &norm_tau_omega_limit)
+
+#ifdef FLIP_FCN
+PARAM_ADD(PARAM_INT16, fr, &flip_roll)
+PARAM_ADD(PARAM_FLOAT, sa, &switch_angle)
+
+
+#endif
+
 PARAM_GROUP_STOP(stabilizer)
 
 
